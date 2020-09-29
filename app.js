6db7d2c2c2907033bfd2f310bfce7618b38d7906
kenchen <independentchen@gmail.com>
2020-09-29 11:52:12 +0800

    Feat: add env variable

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const express = require('express');
-
+const greet = process.env.GREET;
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from docker</h1>');
+  res.send(`<h1>${greet}/h1>`);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
